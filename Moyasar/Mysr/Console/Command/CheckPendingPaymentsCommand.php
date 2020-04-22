dc2df6872dacbd62318780967f61b39d1f0a4d89
Ali Alhoshaiyan <alialhoshaiyan@gmail.com>
2020-04-22 19:49:15 +0300

    Automate Payment Checking

---
+++ Moyasar/Mysr/Console/Command/CheckPendingPaymentsCommand.php
@@ -0,0 +1,230 @@
+<?php
+
+namespace Moyasar\Mysr\Console\Command;
+
+use DateTime;
+use Exception;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\App\Cache\Frontend\Pool;
+use Magento\Framework\App\ObjectManager;
+use Magento\Sales\Api\Data\OrderPaymentInterface;
+use Magento\Sales\Model\Order;
+use Magento\Sales\Model\OrderRepository;
+use Moyasar\Mysr\Helper\Data;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CheckPendingPaymentsCommand extends Command
+{
+    /**
+     * @var InputInterface
+     */
+    protected $input;
+
+    /**
+     * @var OutputInterface
+     */
+    protected $output;
+
+    /**
+     * @var OrderRepository
+     */
+    protected $orderRepository;
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    protected $searchCriteriaBuilder;
+
+    /**
+     * @var Data
+     */
+    protected $dataHelper;
+
+    /**
+     * @var Pool
+     */
+    protected $cachePool;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    public function __construct()
+    {
+        parent::__construct('moyasar:payment:process');
+    }
+
+    /**
+     * Called by the Scheduler
+     */
+    public function cron()
+    {
+        $this->execute(new ArgvInput([]), new ConsoleOutput());
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Process payments for orders with pending status');
+        parent::configure();
+    }
+
+    protected function initServices()
+    {
+        $objectManager = ObjectManager::getInstance();
+
+        $this->orderRepository = $objectManager->get(OrderRepository::class);
+        $this->searchCriteriaBuilder = $objectManager->get(SearchCriteriaBuilder::class);
+        $this->dataHelper = $objectManager->get(Data::class);
+        $this->cachePool = $objectManager->get(Pool::class);
+        $this->logger = $objectManager->get(LoggerInterface::class);
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|void|null
+     */
+    protected function execute($input, $output)
+    {
+        $this->initServices();
+        $orders = $this->getPendingOrders();
+
+        foreach ($orders as $order) {
+            $this->process($order);
+        }
+
+        $output->writeln('Check ' . count($orders) . ' Order/s.');
+    }
+
+    /**
+     * @param Order $order
+     */
+    private function process($order)
+    {
+        if ($order->getState() != Order::STATE_NEW) {
+            return;
+        }
+
+        $cacheKey = 'moyasar-order-checked-' . $order->getId();
+
+        // If already checked within the last 5 minutes, skip
+        if ($this->cache()->load($cacheKey) == 'checked') {
+            return;
+        }
+
+        // Cache order for 5 minutes
+        $this->cache()->save('checked', $cacheKey, [], 60 * 5);
+
+        $this->processPayment($order);
+    }
+
+    /**
+     * @param Order $order
+     * @param OrderPaymentInterface $payment
+     * @return void
+     */
+    private function processPayment($order)
+    {
+        $payment = $order->getPayment();
+
+        if (is_null($payment)) {
+            return;
+        }
+
+        $additionalInfo = $payment->getAdditionalInformation();
+
+        if (! isset($additionalInfo['moyasar_payment_id'])) {
+            $paymentId = $payment->getEntityId();
+            $orderId = $order->getId();
+            $this->logger->warning("Payment ($paymentId) of Order ($orderId) does not have Moyasar payment ID");
+            return;
+        }
+
+        $moyasarPaymentId = $additionalInfo['moyasar_payment_id'];
+
+        $response = null;
+        try {
+            $response = $this->dataHelper->fetchMoyasarPayment($moyasarPaymentId);
+        } catch (Exception $e) {
+            $this->logger->error('Could not fetch Moyasar payment', ['exception' => $e]);
+            return;
+        }
+
+        if (isset($response['type']) && $response['type'] == 'api_error') {
+            $this->logger->error('Could not fetch Moyasar payment ' . $moyasarPaymentId);
+            return;
+        }
+
+        if (! isset($response['status'])) {
+            $this->logger->warning('Unrecognized response from Moyasar', ['response' => $response]);
+            return;
+        }
+
+        $paymentStatus = trim(mb_strtolower($response['status']));
+
+        if ($paymentStatus == 'paid') {
+            if ($this->dataHelper->verifyAmount($order, $moyasarPaymentId, $response)) {
+                $order->addStatusToHistory(Order::STATE_PAYMENT_REVIEW, 'Automated Check: Payment is successful. ID: ' . $moyasarPaymentId);
+                $this->dataHelper->processOrder($order, $moyasarPaymentId);
+            }
+        } elseif ($paymentStatus == 'initiated') {
+            $this->logger->debug('Payment is still new, passing order ' . $order->getId());
+
+            $order->addStatusToHistory(Order::STATE_PAYMENT_REVIEW, 'Automated Check: Payment is still initiated. ID: ' . $moyasarPaymentId);
+            $this->dataHelper->saveOrder($order);
+
+            // The customer still did not pay yet, so we will wait
+            return;
+        } else {
+            // Something is wrong with the payment, cancel the order
+            $message = $moyasarPaymentId;
+
+            if (isset($response['source']['message'])) {
+                $message = $message . '. Moyasar Says: ' . $response['source']['message'];
+            }
+
+            try {
+                if ($this->dataHelper->cancelCurrentOrder($order, $message)) {
+                    $this->logger->info('Order was canceled by automated job', [
+                        'payment_status' => $paymentStatus,
+                        'payment_id' => $moyasarPaymentId,
+                        'order_id' => $order->getId()
+                    ]);
+                }
+            } catch (Exception $e) {
+                $this->logger->error('Could not cancel order automatically', [
+                    'exception' => $e,
+                    'order_id' => $order->getId()
+                ]);
+            }
+        }
+    }
+
+    private function getPendingOrders()
+    {
+        $dateStart = new DateTime();
+        $dateStart->modify('-5 day');
+
+        $dateEnd = new DateTime();
+        $dateEnd->modify('-2 minute');
+
+        $search = $this->searchCriteriaBuilder
+            ->addFilter('state', Order::STATE_NEW)
+            ->addFilter('created_at', $dateStart->format('Y-m-d H:i:s'), 'gteq')
+            ->addFilter('created_at', $dateEnd->format('Y-m-d H:i:s'), 'lteq')
+            ->create();
+
+        return $this->orderRepository->getList($search)->getItems();
+    }
+
+    private function cache()
+    {
+        return $this->cachePool->current();
+    }
+}
--- Moyasar/Mysr/Helper/Data.php
+++ Moyasar/Mysr/Helper/Data.php
@@ -7,9 +7,12 @@ use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\HTTP\Client\Curl;
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\Sales\Api\OrderManagementInterface;
 use Magento\Sales\Model\Order;
+use Magento\Sales\Model\Spi\OrderResourceInterface;
+use Magento\Store\Model\ScopeInterface;
 
 class Data extends AbstractHelper
 {
@@ -17,8 +20,12 @@ class Data extends AbstractHelper
     protected $_objectManager;
     protected $_curl;
 
-    public function __construct(Context $context, OrderManagementInterface $orderManagement, ObjectManagerInterface $objectManager, Curl $curl)
-    {
+    public function __construct(
+        Context $context,
+        OrderManagementInterface $orderManagement,
+        ObjectManagerInterface $objectManager,
+        Curl $curl
+    ) {
         $this->orderManagement = $orderManagement;
         $this->_objectManager = $objectManager;
         $this->_curl = $curl;
@@ -31,7 +38,6 @@ class Data extends AbstractHelper
      *
      * @param Order $order to be saved
      * @return bool True if order saved, false otherwise
-     * @throws Exception
      */
     public function processOrder($order, $id)
     {
@@ -40,9 +46,9 @@ class Data extends AbstractHelper
             $order->setState(Order::STATE_PROCESSING);
             $customerNotified = $this->sendOrderEmail($order);
             $order->addStatusToHistory(Order::STATE_PROCESSING, 'Moyasar Payment Successfully completed. ID: ' . $id, $customerNotified);
-            $order->save();
-            // $invoice = $order->prepareInvoice()->register();
 
+            $this->saveOrder($order);
+            // $invoice = $order->prepareInvoice()->register();
             return true;
         }
         return false;
@@ -81,31 +87,35 @@ class Data extends AbstractHelper
     public function cancelCurrentOrder($order, $id)
     {
         if ($order->getId() && $order->getState() != Order::STATE_CANCELED) {
-            $order->registerCancellation('Moyasar Payment Failed. ID: ' . $id)->save();
+            $order->registerCancellation('Moyasar Payment Failed. ID: ' . $id);
+            $this->saveOrder($order);
             return true;
         }
+
         return false;
     }
 
     /**
      * @param $order Order
      * @param $payment_id
+     * @param array|null $response
      * @return bool
-     * @throws Exception
      */
-    public function verifyAmount($order, $payment_id)
+    public function verifyAmount($order, $payment_id, $response = null)
     {
-        $order_amount = $order->getGrandTotal()*100;
+        $order_amount = $order->getGrandTotal() * 100;
+
         if ($order->getId() && $order->getState() != Order::STATE_PAYMENT_REVIEW) {
             $order->setStatus(Order::STATE_PAYMENT_REVIEW);
             $order->setState(Order::STATE_PAYMENT_REVIEW);
-            $order->addStatusToHistory(Order::STATE_PAYMENT_REVIEW, 'Reviewing payment ID: ' . $payment_id)->save();
+            $order->addStatusToHistory(Order::STATE_PAYMENT_REVIEW, 'Reviewing payment ID: ' . $payment_id);
+            $this->saveOrder($order);
         }
+
         try {
-            $secretApiKey = $this->scopeConfig->getValue('payment/moyasar_cc/secret_api_key', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-            $this->_curl->setCredentials($secretApiKey, '');
-            $this->_curl->get('https://api.moyasar.com/v1/payments/' . $payment_id);
-            $response = json_decode($this->_curl->getBody(), true);
+            if (is_null($response)) {
+                $response = $this->fetchMoyasarPayment($payment_id);
+            }
 
             if (isset($response['message'])) {
                 $this->_logger->addDebug($payment_id . ' Moyasar Payment Verification Failed: ' . $response['message']);
@@ -115,7 +125,8 @@ class Data extends AbstractHelper
             if (isset($response['amount']) && $response['amount'] == $order_amount) {
                 return true;
             } else {
-                $order->addStatusToHistory(Order::STATUS_FRAUD, 'Payment Review Failed: ***possible tampering** | Actual amount paid: ' . $response['amount_format'])->save();
+                $order->addStatusToHistory(Order::STATUS_FRAUD, 'Payment Review Failed: ***possible tampering** | Actual amount paid: ' . $response['amount_format']);
+                $this->saveOrder($order);
                 return false;
             }
         } catch (Exception $e) {
@@ -127,5 +138,33 @@ class Data extends AbstractHelper
     public function getUrl($route, $params = [])
     {
         return $this->_getUrl($route, $params);
+    }
+
+    public function saveOrder(Order $order)
+    {
+        // Save method is deprecated in new versions of Magento
+        if (! class_exists('\Magento\Sales\Model\Spi\OrderResourceInterface')) {
+            $order->save();
+        }
+
+        /** @var OrderResourceInterface $orderResource */
+        $orderResource = ObjectManager::getInstance()->get(OrderResourceInterface::class);
+
+        $orderResource->save($order);
+    }
+
+    public function fetchMoyasarPayment($paymentId)
+    {
+        $secretApiKey = $this->moyasarSecretApiKey();
+
+        $this->_curl->setCredentials($secretApiKey, '');
+        $this->_curl->get("https://api.moyasar.com/v1/payments/$paymentId");
+
+        return json_decode($this->_curl->getBody(), true);
+    }
+
+    public function moyasarSecretApiKey()
+    {
+        return $this->scopeConfig->getValue('payment/moyasar_cc/secret_api_key', ScopeInterface::SCOPE_STORE);
     }
 }
