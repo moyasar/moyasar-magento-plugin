666532c7b5d432ad72c839541b2b513c931a6967
Ali Alhoshaiyan <alialhoshaiyan@gmail.com>
2020-08-18 20:20:36 +0300

    Fixed a bug where the plugin will disable email sending for all orders even if they are not related to moyasar's payment methods

--- Moyasar/Mysr/Observer/BeforeOrderPlaceObserver.php
+++ Moyasar/Mysr/Observer/BeforeOrderPlaceObserver.php
@@ -4,12 +4,24 @@ namespace Moyasar\Mysr\Observer;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Moyasar\Mysr\Model\Payment\MoyasarApplePay;
+use Moyasar\Mysr\Model\Payment\MoyasarCc;
+use Moyasar\Mysr\Model\Payment\MoyasarSadad;
 
 class BeforeOrderPlaceObserver implements ObserverInterface
 {
     public function execute(Observer $observer)
     {
+        $methods = [
+            MoyasarApplePay::CODE,
+            MoyasarCc::CODE,
+            MoyasarSadad::CODE
+        ];
+
         $order = $observer->getOrder();
-        $order->setCanSendNewEmailFlag(false);
+
+        if (in_array($order->getPayment()->getMethod(), $methods)) {
+            $order->setCanSendNewEmailFlag(false);
+        }
     }
 }
